aed8a3ea1e791517f41fa2171bc76d3786e0b6cf
Roshan Kumar <roshankunaguero09@gmail.com>
2021-04-23 16:34:41 +0530

    heroku postgres setting retry part 4

--- config/database.js
+++ config/database.js
@@ -1,7 +1,16 @@
 const Sequelize = require('sequelize');
 let db;
   const postgreUri = 'postgres://biohutpetsbuxn:81f6023d3ff26391a6b290ee7e8f906b9a6fea9ac23a16f7b16874a4cd05cec0@ec2-3-233-7-12.compute-1.amazonaws.com:5432/d8ghc7vsej2q8r'
-  db =  new Sequelize(postgreUri, {
+  // db =  new Sequelize(postgreUri, {
+  //   host: 'ec2-3-233-7-12.compute-1.amazonaws.com',
+  //   dialect: 'postgres',
+  //   pool: {
+  //     max: 5,
+  //     min: 0,
+  //     idle: 10000
+  //   },
+  // });
+  db =  new Sequelize('d8ghc7vsej2q8r', 'biohutpetsbuxn', '81f6023d3ff26391a6b290ee7e8f906b9a6fea9ac23a16f7b16874a4cd05cec0', {
     host: 'ec2-3-233-7-12.compute-1.amazonaws.com',
     dialect: 'postgres',
     pool: {
@@ -10,14 +19,5 @@ let db;
       idle: 10000
     },
   });
-  // db =  new Sequelize('mediumcrawldb', 'postgres', '9939105936', {
-  //   host: 'localhost',
-  //   dialect: 'postgres',
-  //   pool: {
-  //     max: 5,
-  //     min: 0,
-  //     idle: 10000
-  //   },
-  // });
 
 module.exports = db;
