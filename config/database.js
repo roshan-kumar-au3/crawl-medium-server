483cf3d9db9ffc5e4473d82afd79365f97ca0e7e
Roshan Kumar <roshankunaguero09@gmail.com>
2021-04-23 15:16:00 +0530

    server postgres deployment setup

--- config/database.js
+++ config/database.js
@@ -1,13 +1,26 @@
 const Sequelize = require('sequelize');
-
-const db =  new Sequelize('mediumcrawldb', 'postgres', '9939105936', {
-  host: 'localhost',
-  dialect: 'postgres',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  },
-});
+let db;
+if (process.env.PORT) {
+  const postgreUri = 'postgres://biohutpetsbuxn:81f6023d3ff26391a6b290ee7e8f906b9a6fea9ac23a16f7b16874a4cd05cec0@ec2-3-233-7-12.compute-1.amazonaws.com:5432/d8ghc7vsej2q8r'
+  db =  new Sequelize(postgreUri, {
+    host: 'ec2-3-233-7-12.compute-1.amazonaws.com',
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    },
+  });
+} else {
+  db =  new Sequelize('mediumcrawldb', 'postgres', '9939105936', {
+    host: 'localhost',
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    },
+  });
+}
 
 module.exports = db;
