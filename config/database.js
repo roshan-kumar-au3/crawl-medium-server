dc2074bddb3e2df14c1d52669d9bba56afd22d0a
Roshan Kumar <roshankunaguero09@gmail.com>
2021-04-23 17:44:52 +0530

    deployment configuration done

--- config/database.js
+++ config/database.js
@@ -1,31 +1,32 @@
 const Sequelize = require('sequelize');
 let db;
   const postgreUri = 'postgres://biohutpetsbuxn:81f6023d3ff26391a6b290ee7e8f906b9a6fea9ac23a16f7b16874a4cd05cec0@ec2-3-233-7-12.compute-1.amazonaws.com:5432/d8ghc7vsej2q8r'
-  // db =  new Sequelize(postgreUri, {
-  //   host: 'ec2-3-233-7-12.compute-1.amazonaws.com',
-  //   dialect: 'postgres',
-  //   pool: {
-  //     max: 5,
-  //     min: 0,
-  //     idle: 10000
-  //   },
-  // });
-  db =  new Sequelize(postgreUri, {
-    // host: 'localhost',
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false // <<<<<<< YOU NEED THIS
-      }
-    },
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000
-    },
-  });
-  // db =  new Sequelize(postgreUri);
+  if (process.env.NODE_ENV === 'production') {
+    db =  new Sequelize(postgreUri, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      },
+      pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+      },
+    });
+  } else {
+    db =  new Sequelize("mediumcrawldb", "postgres", "9939105936", {
+      host: 'localhost',
+      dialect: 'postgres',
+      pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+      },
+    });
+  }
 
 
 module.exports = db;
