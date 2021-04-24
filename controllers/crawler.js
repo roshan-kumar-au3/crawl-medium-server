fc2d54e49e6964862596fc50c922396627393793
Roshan Kumar <roshankunaguero09@gmail.com>
2021-04-24 15:23:40 +0530

    handling edge case topic page not available on medium

--- controllers/crawler.js
+++ controllers/crawler.js
@@ -63,58 +63,64 @@ const crawlMediumMiddleware = (req, res, next) => {
 const crawlMedium = (req, res) => {
     const searchTag = req.query.tag.toLowerCase().replace(/\s+/g, '-');
     const mediumUrl = `https://medium.com/topic/${searchTag}`;
-    request( mediumUrl, (error, response, html) => {
-        if (!error && response.statusCode == 200) {
-            const $ = cheerio.load(html);
-            let crawlData = [];
-            const blogSection = $('section > div > section');
-            blogSection.each((i, el) => {
-                const descriptionData = $(el).children().first().text();
-                const crawlObj = {
-                    link:  $(el).find('a').attr('href'),
-                    description:  $(el).find('a').text().substr(0, descriptionData.length - 1),
-                    author: descriptionData.substr(descriptionData.length - 44, descriptionData.length - 17),
-                    readInfo: descriptionData.substr(descriptionData.length - 17, descriptionData.length)
-                }
-                if (crawlObj.link.startsWith('/')) {
-                    crawlObj.link = `https://medium.com${crawlObj.link}`
-                }
-                request(crawlObj.link, (error, response, html) => {
-                  if (!error && response.statusCode == 200) {
-                    const $ = cheerio.load(html);
-                    const title = $('h1').text();
-                    let author = $('a').children('p').first().text();
-                    console.log("author", author)
-                    crawlObj.title = title;
-                    crawlObj.description = descriptionData.substr(title.length, descriptionData.length - 1)
-                    crawlObj.author = author;
-                    if (crawlObj.title) {
-                        crawlData.push(crawlObj);
-                        console.log(crawlObj);                   
+    try {
+          request( mediumUrl, (error, response, html) => {
+            if (!error && response.statusCode == 200) {
+                const $ = cheerio.load(html);
+                let crawlData = [];
+                const blogSection = $('section > div > section');
+                blogSection.each((i, el) => {
+                    const descriptionData = $(el).children().first().text();
+                    const crawlObj = {
+                        link:  $(el).find('a').attr('href'),
+                        description:  $(el).find('a').text().substr(0, descriptionData.length - 1),
+                        author: descriptionData.substr(descriptionData.length - 44, descriptionData.length - 17),
+                        readInfo: descriptionData.substr(descriptionData.length - 17, descriptionData.length)
                     }
-                    // console.log('crawl dataaaaaaa', crawlData)
-                    // res.write(crawlData);
-                    if (crawlData.length === 5) {
-                       return  res.status(200).json({
-                            message: 'success',
-                            crawlData
-                        });
+                    if (crawlObj.link.startsWith('/')) {
+                        crawlObj.link = `https://medium.com${crawlObj.link}`
                     }
-                  }
+                    request(crawlObj.link, (error, response, html) => {
+                      if (!error && response.statusCode == 200) {
+                        const $ = cheerio.load(html);
+                        const title = $('h1').text();
+                        let author = $('a').children('p').first().text();
+                        console.log("author", author)
+                        crawlObj.title = title;
+                        crawlObj.description = descriptionData.substr(title.length, descriptionData.length - 1)
+                        crawlObj.author = author;
+                        if (crawlObj.title) {
+                            crawlData.push(crawlObj);
+                            console.log(crawlObj);                   
+                        }
+                        // console.log('crawl dataaaaaaa', crawlData)
+                        // res.write(crawlData);
+                        if (crawlData.length === 5) {
+                          return  res.status(200).json({
+                                message: 'success',
+                                crawlData
+                            });
+                        }
+                      }
+                    });
                 });
-            });
-        }
-        if (!html) {
-          return res.status(400).json({
-            error: 'No blog found with the the given topic'
-        })
-        }
-        if (error) {
-            return res.status(400).json({
-                error: 'Not able to scrape'
+            }
+            if (!html) {
+              return res.status(400).json({
+                error: 'No blog found with the the given topic'
             })
-        }
-    });
+            }
+            if (error) {
+                return res.status(400).json({
+                    error: 'Not able to scrape'
+                })
+            }
+        });
+    } catch (error) {
+      return res.status(400).json({
+        error: 'No blog found with the the given topic'
+      })
+    }
 }
 
 
